b0baf53155296c9931cb1d974e0464c645d784b7
Tarcísio Lima Ferreira <tarcisiolf0@gmail.com>
2024-10-16 21:00:44 -0300

    Create insertion_sort.py

---
+++ chapter1/insertion_sort.py
@@ -0,0 +1,41 @@
+array1 = [5, 2, 4, 6, 1, 3]
+
+array2 = [31, 41, 59, 26, 41, 58]
+
+def insertion_sort_increasing(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+
+        print("I {} KEY {} J {} A[j] {}".format(i, key, j, array[j]))
+
+        while j >= 0 and array[j] > key:
+            array[j+1] = array[j]
+            j -= 1
+            print("cond", array)
+
+        array[j+1] = key
+
+        print(array)
+
+    return array
+
+def insertion_sort_decreasing(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+
+        print("I {} KEY {} J {} A[j] {}".format(i, key, j, array[j]))
+
+        while j >= 0 and array[j] < key:
+            array[j+1] = array[j]
+            j -= 1
+            print("cond", array)
+
+        array[j+1] = key
+
+        print(array)
+
+    return array
+
+insertion_sort_decreasing(array1)
